813164d82b6c8841945a470e73b8d8c7eae56abb
Sean Walker <swlkr.rbl@gmail.com>
2019-08-06 17:48:13 -0600

    Add connector and connector-test

---
+++ src/db/connector.clj
@@ -0,0 +1,79 @@
+(ns db.connector
+  (:require [clojure.edn :as edn]
+            [clojure.java.io :as io]
+            [env.core :as env])
+  (:import (com.zaxxer.hikari HikariConfig HikariDataSource)
+           (java.util Properties)
+           (java.io FileNotFoundException)))
+
+
+(defn db-edn []
+  (let [filename "db.edn"
+        resource (io/resource filename)]
+    (slurp (or resource filename))))
+
+
+(defn context
+  ([db-env]
+   (let [ctx (->> (db-edn)
+                  (edn/read-string {:readers {'env env/env}})
+                  (mapv (fn [[k v]] [k v]))
+                  (into {}))]
+      (get ctx (or db-env :dev))))
+  ([]
+   (context :dev)))
+
+
+(def opts {:auto-commit        true
+           :read-only          false
+           :connection-timeout 30000
+           :validation-timeout 5000
+           :idle-timeout       600000
+           :max-lifetime       1800000
+           :minimum-idle       10
+           :maximum-pool-size  10
+           :register-mbeans    false})
+
+
+(defn pool
+  "Shamelessly stolen from tomekw/hikari-cp. Makes a new hikaricp data source"
+  [ctx]
+  (let [ctx (merge opts ctx)
+        {:keys [adapter database]} ctx
+        connection-init-sql (when (= "sqlite" adapter)
+                              "PRAGMA foreign_keys=ON")
+        data-sources {"sqlite" "org.sqlite.SQLiteDataSource"
+                      "postgres" "org.postgresql.ds.PGSimpleDataSource"}
+        datasource-class-name (get data-sources adapter)
+        _ (when (nil? datasource-class-name)
+            (throw (Exception. "Unsupported connection string, only sqlite and postgres are supported currently")))
+        c (doto (HikariConfig.)
+            (.setDataSourceClassName datasource-class-name)
+            (.addDataSourceProperty  "databaseName" database)
+            (.setUsername            (:username ctx))
+            (.setPassword            (:password ctx))
+            (.setAutoCommit          (:auto-commit ctx))
+            (.setReadOnly            (:read-only ctx))
+            (.setConnectionTimeout   (:connection-timeout ctx))
+            (.setValidationTimeout   (:validation-timeout ctx))
+            (.setIdleTimeout         (:idle-timeout ctx))
+            (.setMaxLifetime         (:max-lifetime ctx))
+            (.setMinimumIdle         (:minimum-idle ctx))
+            (.setMaximumPoolSize     (:maximum-pool-size ctx))
+            (.setConnectionInitSql   connection-init-sql))]
+       _ (when (= "sqlite" adapter)
+           (.addDataSourceProperty c "url" (str "jdbc:sqlite:" database)))
+       _ (when (some? (:port ctx))
+           (.addDataSourceProperty c "portNumber" (:port ctx)))
+       _ (when (some? (:host ctx))
+           (.addDataSourceProperty c "serverName" (:host ctx)))
+    (merge {:datasource (HikariDataSource. c)}
+      ctx)))
+
+
+(defn connect [ctx]
+  (pool ctx))
+
+
+(defn disconnect [connection]
+  (.close (:datasource connection)))
---
+++ test/db/connector_test.clj
@@ -0,0 +1,51 @@
+(ns db.connector-test
+  (:require [db.connector :as connector]
+            [clojure.test :refer :all]
+            [clojure.java.io :as io]
+            [db.core])
+  (:import (java.io FileNotFoundException)))
+
+(def context {:adapter "sqlite" :database "test.sqlite3"})
+
+(defn with-db [f]
+  (f)
+  (Thread/sleep 100)
+  (db.core/drop context)
+  (when (.exists (io/file "db.edn"))
+    (io/delete-file "db.edn")))
+
+(use-fixtures :once with-db)
+
+(deftest context-test
+  (testing "db-edn without a db.edn"
+    (when (.exists (io/file "db.edn"))
+      (io/delete-file "db.edn"))
+
+    (is (thrown? FileNotFoundException (connector/db-edn))))
+
+  (testing "with db.edn"
+    (spit "db.edn" "{:dev {:adapter \"sqlite\"}}")
+    (is (= "sqlite" (:adapter (connector/context)))))
+
+  (testing "with db.edn and :test"
+    (spit "db.edn" "{:test {:adapter \"postgres\"}}")
+    (is (= "postgres" (:adapter (connector/context :test))))))
+
+
+(deftest pool-test
+  (testing "pool with sqlite"
+    (is (true? (contains? (connector/pool context)
+                 :datasource)))))
+
+
+(deftest connect-test
+  (testing "connect with sqlite"
+    (is (true? (contains?
+                 (connector/connect context)
+                 :datasource)))))
+
+
+(deftest disconnect-test
+  (testing "disconnecting with sqlite"
+    (let [connection (connector/connect context)]
+      (is (nil? (connector/disconnect connection))))))
